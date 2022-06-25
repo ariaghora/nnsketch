792a72b00ce55cdec7080f98b8caf4ab12e261f8
Aria Ghora Prabono <hello@ghora.net>
2022-06-25 21:06:04 +0700

    Initial project list dev

---
+++ webui/src/lib/ProjectList.svelte
@@ -0,0 +1,36 @@
+<script>
+    import ProjectListItem from "./ProjectListItem.svelte";
+
+    const projectList = [
+        { projectName: "Simple MLP", projectId: "1" },
+        { projectName: "Example CNN", projectId: "2" },
+        { projectName: "Project 3", projectId: "3" },
+        { projectName: "Project 4", projectId: "4" },
+        { projectName: "Project 5", projectId: "5" },
+    ];
+</script>
+
+<!-- Create a grid for project list item -->
+<div class="project-list">
+    {#each projectList as project}
+        <ProjectListItem
+            projectName={project.projectName}
+            projectId={project.projectId}
+        />
+    {/each}
+</div>
+
+<style lang="scss">
+    .project-list {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 0.2fr));
+        grid-gap: 40px;
+        margin: 20px;
+        padding: 20px;
+        border-radius: 5px;
+        font-size: 14px;
+        font-weight: normal;
+        text-align: left;
+        cursor: pointer;
+    }
+</style>
---
+++ webui/src/lib/ProjectListItem.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    export let projectId;
+    export let projectName;
+</script>
+
+<div class="project-list-item">
+    <div class="preview">
+        <!-- preview image -->
+    </div>
+    <span class="project-list-item-name">
+        {projectName}
+    </span>
+</div>
+
+<style lang="scss">
+    .project-list-item {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 200px;
+        height: 120px;
+        border-radius: 5px;
+        background-image: var(--gradient-1);
+        padding: 10px;
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
+        cursor: pointer;
+        .project-list-item-name {
+            font-family: var(--monospace-font);
+            font-size: 14px;
+            font-weight: bold;
+            text-align: center;
+        }
+        .preview {
+            width: 100%;
+            height: 100%;
+            background-color: #333;
+            border-radius: 5px;
+        }
+    }
+</style>
